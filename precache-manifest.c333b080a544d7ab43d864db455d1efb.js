da0677bb5b56b43bde0f5f8709b4292153d00e10
Nedomorf <danikp01@,ail.ru>
2020-10-04 22:14:13 +0300

    Updates

---
+++ precache-manifest.c333b080a544d7ab43d864db455d1efb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16b64e25bc3fff3fa1f4b527659279c",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "79d9add467cf3e9e72bc",
+    "url": "/recipes/static/css/main.6632a14b.chunk.css"
+  },
+  {
+    "revision": "e835678a7942fd02c4d7",
+    "url": "/recipes/static/js/2.5a36c6ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipes/static/js/2.5a36c6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79d9add467cf3e9e72bc",
+    "url": "/recipes/static/js/main.e8f50575.chunk.js"
+  },
+  {
+    "revision": "65aed4a3aaa500d440aa",
+    "url": "/recipes/static/js/runtime-main.9fddb04f.js"
+  },
+  {
+    "revision": "8ed8db8c7de7fc73e523be452c96b56b",
+    "url": "/recipes/static/media/bg.8ed8db8c.mp4"
+  }
+]);
